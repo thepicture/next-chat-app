1ebade5e07e0fda03ae2ba9d8a41f2dd3afdef99
thepicture <85333076+thepicture@users.noreply.github.com>
2022-07-19 16:53:03 +0500

    feat: add login validation hint

--- pages/login.tsx
+++ pages/login.tsx
@@ -11,6 +11,7 @@ export interface Credentials {
 }
 
 const Login: NextPage = () => {
+  const [error, setError] = useState(false);
   const [credentials, setCredentials] = useState<Credentials>({
     email: "",
     password: "",
@@ -19,15 +20,17 @@ const Login: NextPage = () => {
     const { type, value } = e.target;
     setCredentials({ ...credentials, [type]: value });
   };
-  const handleLogin = async () => {
-    const response = await axios.post("/api/login", credentials, {
-      validateStatus: (status) => status === 200 || status === 401,
-    });
-    if (response.data?.username) {
-      alert("You are logged in");
-    } else {
-      alert("Incorrect email or password");
-    }
+  const handleLogin = () => {
+    axios
+      .post("/api/login", credentials)
+      .then(() => {
+        setError(false);
+        alert("You are logged in");
+      })
+      .catch(() => {
+        setError(true);
+        alert("Incorrect email or password");
+      });
   };
   return (
     <div className={styles.container}>
@@ -45,19 +48,21 @@ const Login: NextPage = () => {
         <Card sx={{ p: 4 }}>
           <Stack>
             <Typography component="h1" variant="h4">
-              Enter your account
+              Login
             </Typography>
             <TextField
               onChange={handleChange}
+              error={error}
               type="email"
               placeholder="Email"
             />
             <TextField
               onChange={handleChange}
+              error={error}
               type="password"
               placeholder="Password"
             />
-            <Button onClick={handleLogin}>Enter</Button>
+            <Button onClick={handleLogin}>Sign in</Button>
           </Stack>
         </Card>
       </Box>
