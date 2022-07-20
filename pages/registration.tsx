ef54cfc1c286e0958fb22b34afd1792df38aafaa
thepicture <85333076+thepicture@users.noreply.github.com>
2022-07-20 18:55:20 +0500

    feat(registration): add client-side validation

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -7,27 +7,20 @@ import { ChangeEvent, useState } from "react";
 import AlertDialog from "../components/AlertDialog";
 import styles from "../styles/Login.module.sass";
 import { Credentials } from "./login";
+import { ErrorMessage, Formik, FormikHelpers, FormikValues } from "formik";
 
 export interface RegistrationCredentials extends Credentials {
   username: string;
 }
 
+const emailRegExpPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 const Login: NextPage = () => {
   const [message, setMessage] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [error, setError] = useState(false);
-  const [credentials, setCredentials] = useState<RegistrationCredentials>({
-    email: "",
-    username: "",
-    password: "",
-  });
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setCredentials({ ...credentials, [name]: value });
-  };
-  const handleLogin = () => {
+  const handleRegistration = (values: FormikValues) => {
     axios
-      .post("/api/registration", credentials)
+      .post("/api/registration", values)
       .then(() => {
         setError(false);
         setMessage("You are registered!");
@@ -57,36 +50,73 @@ const Login: NextPage = () => {
           height="100vh"
         >
           <Card sx={{ p: 4 }}>
-            <Stack>
-              <Typography component="h1" variant="h4">
-                Enter your new account's info
-              </Typography>
-              <TextField
-                onChange={handleChange}
-                error={error}
-                type="email"
-                name="email"
-                placeholder="Email"
-              />
-              <TextField
-                onChange={handleChange}
-                error={error}
-                type="text"
-                name="username"
-                placeholder="Username"
-              />
-              <TextField
-                onChange={handleChange}
-                error={error}
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
-              <Button onClick={handleLogin}>Register</Button>
-              <Link href="/login">
-                <a style={{ textAlign: "center" }}>I have an account</a>
-              </Link>
-            </Stack>
+            <Formik
+              initialValues={{ email: "", username: "", password: "" }}
+              validate={(values) => {
+                const errors: RegistrationCredentials =
+                  {} as RegistrationCredentials;
+                if (!values.email) {
+                  errors.email = "Email required";
+                } else if (!emailRegExpPattern.test(values.email)) {
+                  errors.email = "Enter valid email address";
+                }
+                if (!values.username) errors.username = "Username required";
+                if (!values.password) errors.password = "Password required";
+                else if (values.password.length < 5)
+                  errors.password =
+                    "Password length must be at least 5 characters long";
+
+                return errors;
+              }}
+              onSubmit={(values) => handleRegistration(values)}
+            >
+              {({ values, handleChange, handleSubmit }) => (
+                <form onSubmit={handleSubmit}>
+                  <Stack>
+                    <Typography component="h1" variant="h4">
+                      Enter your new account's info
+                    </Typography>
+                    <TextField
+                      onChange={handleChange}
+                      value={values.email}
+                      error={error}
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                    />
+                    <Typography component="p">
+                      <ErrorMessage name="email" />
+                    </Typography>
+                    <TextField
+                      onChange={handleChange}
+                      value={values.username}
+                      error={error}
+                      type="text"
+                      name="username"
+                      placeholder="Username"
+                    />{" "}
+                    <Typography component="p">
+                      <ErrorMessage name="username" />
+                    </Typography>
+                    <TextField
+                      onChange={handleChange}
+                      value={values.password}
+                      error={error}
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                    />
+                    <Typography component="p">
+                      <ErrorMessage name="password" />
+                    </Typography>
+                    <Button type="submit">Register</Button>
+                    <Link href="/login">
+                      <a style={{ textAlign: "center" }}>I have an account</a>
+                    </Link>
+                  </Stack>
+                </form>
+              )}
+            </Formik>
           </Card>
         </Box>
       </div>
